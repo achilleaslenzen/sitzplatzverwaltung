ea953bda1c72f4b6d51d0b795517efe4f54d4723
Achilleas Lenzen <achilleas.lenzen@gmx.de>
2024-11-17 17:29:48 +0100

    implement drop zone and arbeitsplaaetze draggable

--- src/app/werkstatt/werkstatt.component.ts
+++ src/app/werkstatt/werkstatt.component.ts
@@ -32,15 +32,20 @@ export class WerkstattComponent implements AfterViewInit {
   container!: ElementRef<HTMLDivElement>;
 
   @ViewChildren('raum') raumElements!: QueryList<ElementRef>;
+  @ViewChildren('arbeitsplatz') arbeitsplatzElements!: QueryList<ElementRef>;
 
   raeume: Raum[] = [];
+  tische: Tisch[] = [];
 
   constructor() {}
 
   ngAfterViewInit(): void {
     this.raumElements.changes.subscribe(() => {
-      this.raumElements.forEach((raumElement) =>
-        this.initializeInteract(raumElement)
+      this.raumElements.forEach((raumElement) => this.initRaum(raumElement));
+    });
+    this.arbeitsplatzElements.changes.subscribe(() => {
+      this.arbeitsplatzElements.forEach((element) =>
+        this.initArbeitsplatz(element)
       );
     });
   }
@@ -54,7 +59,107 @@ export class WerkstattComponent implements AfterViewInit {
     console.log(this.raeume);
   }
 
-  initializeInteract(elementRef: ElementRef) {
+  addArbeitsplatz() {
+    const newTisch: Tisch = {
+      besetzt: false,
+      nummer: 1,
+    };
+    this.tische = [...this.tische, newTisch];
+  }
+
+  initRaum(elementRef: ElementRef) {
+    interact(elementRef.nativeElement)
+      .resizable({
+        edges: { left: true, right: true, bottom: true, top: true },
+        modifiers: [
+          interact.modifiers.restrictEdges({
+            outer: 'parent',
+          }),
+          interact.modifiers.restrictSize({
+            min: { width: 100, height: 50 },
+          }),
+        ],
+        listeners: {
+          move: this.resizeMoveListener,
+        },
+        inertia: true,
+      })
+      .draggable({
+        inertia: true,
+        modifiers: [
+          interact.modifiers.restrictRect({
+            restriction: 'self',
+            endOnly: true,
+          }),
+          interact.modifiers.restrictEdges({
+            outer: 'parent',
+          }),
+        ],
+        autoScroll: true,
+        listeners: {
+          move: this.dragMoveListener,
+
+          end: this.dragEndListener,
+        },
+      })
+      .dropzone({
+        accept: '.arbeitsplatz-container',
+        ondropactivate: this.onDropActivateListener,
+        ondragenter: this.onDragEnterListener,
+        ondragleave: this.onDragLeaveListener,
+        ondrop: this.onDropListener,
+        ondropdeactivate: this.onDropDeactivateListener,
+      });
+  }
+
+  onDropActivateListener = (event: any) => {
+    console.log('entered the drop zone');
+    event.target.classList.add('drop-active');
+  };
+
+  onDragEnterListener = (event: any) => {
+    const draggableElement = event.relatedTarget;
+    const dropzoneElement = event.target;
+
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+  };
+
+  onDragLeaveListener = (event: any) => {
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+  };
+
+  onDropListener = (event: any) => {
+    const raumElement = event.target;
+    const raumId = raumElement.getAttribute('data-id');
+
+    const arbeitsplatzElement = event.relatedTarget;
+    const tischNummer = arbeitsplatzElement.getAttribute('data-nummer');
+
+    // Finde den Raum in der Liste
+    const raum = this.raeume.find((r) => r.id === parseInt(raumId, 10));
+    if (raum) {
+      const neuerTisch: Tisch = {
+        nummer: parseInt(tischNummer, 10),
+        besetzt: false,
+      };
+      raum.reihen[0].tische.push(neuerTisch);
+      console.log(
+        `Arbeitsplatz ${tischNummer} wurde dem Raum ${raumId} hinzugefügt.`
+      );
+    } else {
+      console.error('Raum nicht gefunden!');
+    }
+  };
+
+  onDropDeactivateListener = (event: any) => {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  };
+
+  initArbeitsplatz(elementRef: ElementRef) {
     interact(elementRef.nativeElement)
       .resizable({
         edges: { left: true, right: true, bottom: true, top: true },
@@ -91,7 +196,22 @@ export class WerkstattComponent implements AfterViewInit {
       });
   }
 
-  resizeMoveListener(event: any) {
+  dragMoveListener = (event: any) => {
+    const target = event.target;
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    target.style.transform = `translate(${x}px, ${y}px)`;
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  };
+
+  dragEndListener = (event: any) => {
+    console.log('Dragging ended', event);
+  };
+
+  resizeMoveListener = (event: any) => {
     const target = event.target;
     let x = parseFloat(target.getAttribute('data-x')) || 0;
     let y = parseFloat(target.getAttribute('data-y')) || 0;
@@ -105,20 +225,5 @@ export class WerkstattComponent implements AfterViewInit {
     target.style.transform = `translate(${x}px, ${y}px)`;
     target.setAttribute('data-x', x.toString());
     target.setAttribute('data-y', y.toString());
-  }
-
-  dragMoveListener(event: any) {
-    const target = event.target;
-    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    target.style.transform = `translate(${x}px, ${y}px)`;
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-
-  dragEndListener(event: any) {
-    console.log('Dragging ended', event);
-  }
+  };
 }
